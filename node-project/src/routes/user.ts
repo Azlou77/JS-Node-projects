b1b565561f70f218eb04960e58b4442ad3dc56fb
Nguyen Louis <nguyenlouis172@gmail.com>
2023-01-25 09:10:50 +0100

    crud with database online

--- node-project/src/routes/user.ts
+++ node-project/src/routes/user.ts
@@ -4,6 +4,7 @@ const app = express.Router()
 
 app.get('/user', (req, res) => {
   // console.log(req.headers.authorization)
+  console.log(req.user)
   res.status(200).json({ message: 'Hello user' })
 })
 
