04fe261aec1e184d039dede4d4a86b40dd9235ff
Nguyen Louis <nguyenlouis172@gmail.com>
2023-01-26 14:06:15 +0100

    create new post

--- node-project/src/routes/comment.ts
+++ node-project/src/routes/comment.ts
@@ -4,91 +4,94 @@ import db from "../db";
 
 const app = Router()
 
-
-app.get('/todos', async (req, res) => {
-  const todos = await db.todoList.findMany({
-    where: {
-      userId: req.user.id
-    },
-    include: {
-      todoItems: true
-    }
-  })
-  return res.status(200).json(todos)
-})
-
-app.get(
-  '/todo/:uuid',
-  async (req, res) => {
-    try {
-      const todo = await db.todoList.findFirstOrThrow({
-        where: {
-          id: req.params.uuid,
-          userId: req.user.id
-        },
-        include: {
-          todoItems: true
-        }
-      })
-
-      return res.status(200).json(todo)
-    } catch(e) {
-      return res.status(400).json({ message: 'Not found' })
-    }
-  }
-)
+//Request to display all comments
+// app.get('/post', async (req, res) => {
+//   const post = await db.post.findMany({
+//     where: {
+//       authorId: req.user.id
+//     },
+//     include: {
+//       Comment: true
+//     }
+//   })
+//   return res.status(200).json(post)
+// })
 
 
-app.post(
-  '/todo',
-  body('name').exists().isString().notEmpty(),
+//Request to display a specific comment by id
+// app.get(
+//   '/todo/:uuid',
+//   async (req, res) => {
+//     try {
+//       const todo = await db.post.findFirstOrThrow({
+//         where: {
+//           id: req.params.uuid,
+//           authorId: req.user.id
+//         },
+//         include: {
+//           Comment: true
+//         }
+//       })
+
+//       return res.status(200).json(todo)
+//     } catch(e) {
+//       return res.status(400).json({ message: 'Not found' })
+//     }
+//   }
+// )
+
+  //Request to create a new post
+  app.post(
+    '/post',
+  body('title').exists().isString().notEmpty(),
   async (req: Request, res: Response) => {
     try {
       validationResult(req).throw()
-      const createdTodoList = await db.todoList.create({
+      const createPost = await db.post.create({
         data: {
-          name: req.body.name,
-          userId: req.user.id
+          title: req.body.title,
+          authorId: req.user.id,
         }
       })
 
-      return res.status(200).json(createdTodoList)
+      return res.status(200).json(createPost)
     } catch(e) {
       console.log(e)
-      return res.status(400).json({error: e || 'Cannot create todoList'})
+      return res.status(400).json({error: e || 'Cannot create Post'})
     }
 })
 
-app.put('/todo/:uuid', body('name').exists().isString().notEmpty(), async (req, res) => {
-  try {
-    validationResult(req).throw()
-    const updatedTodoList = await db.todoList.update({
-      where: {
-        id: req.params?.uuid,
-      },
-      data: {
-        name: req.body.name
-      }
-    })
 
-    return res.status(200).json(updatedTodoList)
-  } catch(e) {
-    return res.status(400).json({message: e || 'Error while updating'})
-  }
-})
+// app.put('/todo/:uuid', body('name').exists().isString().notEmpty(), async (req, res) => {
+//   try {
+//     validationResult(req).throw()
+//     const updatedTodoList = await db.todoList.update({
+//       where: {
+//         id: req.params?.uuid,
+//       },
+//       data: {
+//         name: req.body.name
+//       }
+//     })
 
-app.delete('/todo/:uuid', async (req, res) => {
-  try {
-    await db.todoList.delete({
-      where: {
-        id: req.params.uuid
-      }
-    })
+//     return res.status(200).json(updatedTodoList)
+//   } catch(e) {
+//     return res.status(400).json({message: e || 'Error while updating'})
+//   }
+// })
 
-    return res.status(200).json({message: `Succesfully deleted ${req.params.uuid}`})
-  } catch(e) {
-    return res.status(400).json({message: e || 'Error while deleting'})
-  }
-})
+// app.delete('/todo/:uuid', async (req, res) => {
+//   try {
+//     await db.todoList.delete({
+//       where: {
+//         id: req.params.uuid
+//       }
+//     })
+
+//     return res.status(200).json({message: `Succesfully deleted ${req.params.uuid}`})
+//   } catch(e) {
+//     return res.status(400).json({message: e || 'Error while deleting'})
+//   }
+// })
 
 export default app
