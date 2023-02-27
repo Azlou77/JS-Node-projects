3ca4ad9ea1a4554198d898a61c6b266c60d0cf0a
Azlou77 <nguyenlouis172@gmail.com>
2023-02-27 17:05:34 +0100

    add data companies paris

---
+++ apimap/data/paris.js
@@ -0,0 +1,617 @@
+var parisJSON = 
+{
+    
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Initiale",
+              "addresse": "3 rue de Sévigné",
+              "color": "",
+              "marker-size": "medium",
+              "amenity": "",
+              "popupContent": ""
+            },
+            "geometry": {
+              "coordinates": [
+                2.3617409,
+                48.8553094
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Covent garden",
+              "addresse": "12 Cité Falguière",
+              "color": "",
+              "marker-size": "medium",
+              "amenity": "",
+              "popupContent": ""
+            },
+            "geometry": {
+              "coordinates": [
+                2.312139,
+                48.840735
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Innoven Partenaires",
+              "addresse": "1 Rue de la Paix",
+              "color": "",
+              "marker-size": "medium",
+              "amenity": "",
+              "popupContent": ""
+            },
+            "geometry": {
+              "coordinates": [
+                6.8655452,
+                47.6310777
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Architecture Catb Batiment",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium",
+              "addresse": "11 rue Clapeyron"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3239638,
+                48.8816164
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "AstoriaFinance",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium",
+              "addresse": "5/7 rue de Monttessuy"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3006776,
+                48.8598413
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Alliance Immobiliere \t",
+              "addresse": "14, rue des Deux Ponts",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3567388,
+                48.8519511
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name ": "Fleming France",
+              "addrese": "39/41 rue Cambron"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3274294, 
+                48.8689199
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Grafikmente",
+              "addresse": "118, rue Réaumur",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3449499,
+                48.8679674
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Idylis",
+              "addresse": "10, rue Vandrezann",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3548496,
+                48.8281573
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Cap Express",
+              "addresses": "11bis, rue Clapeyron",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3239,
+                48.8816
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Mkimmo13 Saint Marcel \t",
+              "addresse": "15, Boulevard Saint Marcel",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3597395,
+                48.8390999
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Matière",
+              "addresse": "1, place d'Iéna",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2942992,
+                48.8645472
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "CHL",
+              "addresse": "71, avenue Marceau",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.297199,
+                48.8709467
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Un appartement à Paris",
+              "addresse": "44-46, rue de la Bienfaisance "
+            },
+            "geometry": {
+              "coordinates": [
+                2.3154742,
+                48.876554
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Arts Immobilier",
+              "addresse": "39, rue de Vinaigriers",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3616388,
+                48.8731191
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "ACE INFORMATIQUE",
+              "addresse": ""
+            },
+            "geometry": {
+              "coordinates": [
+                2.487463275068933,
+                49.13388780454801
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Foncia EOL Etude Opéra Lafayette ",
+              "addresse": "96 rue Faubourg Poissonière",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3471132,
+                48.8709166
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Michèle Lasserre",
+              "addresse": "\t127, rue de Rome "
+            },
+            "geometry": {
+              "coordinates": [
+                2.3171304,
+                48.8853584
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " \tAllure reception",
+              "addresse": "55 rue de Lhomond",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3486195,
+                48.8410697
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Joar Transaction",
+              "addresse": "23 avenue Leopold II",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2712185,
+                48.8525547
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Les Papeteries Réaumur",
+              "addresse": "116 rue Réaumur",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3451822,
+                48.8679165
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Pierre et Vacances ",
+              "addresse": "11 Rue de Cambrai, 75947 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3797426,
+                48.8960154
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "GAT RIMON ",
+              "addresse": "15 Rue de la Grange Batelière, 75009 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3415787,
+                48.8960154
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "District Ile Saint Louis",
+              "addresse": "31 Rue des Deux Ponts, 75004 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3567203,
+                48.852143
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "SAS Pitcho Fassina Petkova",
+              "addresse": "42 Rue Ampère, 75017 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.301453,
+                48.8853741
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Realis Paris",
+              "addresse": "23 RUE D'ANJOU 75008 PARIS 8",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3224401,
+                48.8734865
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "HILDE IMMOBILIER ",
+              "addresse": "66 Bd Saint-Germain, 75005 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3477035,
+                48.8504294
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "SCI Delisa ",
+              "addresse": "59 Bd Lannes, 75116 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2705983,
+                48.8650493
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " DISTRICT BOSQUET CHAMPS DE MARS ",
+              "addresse": "56 Av. Bosquet, 75007 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.304897,
+                48.855356
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Pierre Katz & Co",
+              "addresse": "56 Av. Bosquet, 75007 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.304897,
+                48.855356
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "MP PRODUCTIONS",
+              "addresse": "5 PL VICTOR HUGO 75116 PARIS ",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2856793,
+                48.8692799
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Restaurants programms",
+              "addresse": "31 avenue des Champs Élysée",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                3.8977137,
+                45.0528945
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " Waooou",
+              "addresse": "12, rue Vivienne 75002 ",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3394491,
+                48.8676307
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Marbeuf Optique ",
+              "addresse": "28 rue Marbeuf 75008 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3394491,
+                48.8676307
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "BEAUCHANT ASSOCIES CONSEIL",
+              "addresse": "9 rue de la Trémoille 75008 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.302885,
+                48.8670745
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " COTOLAINE ",
+              "addresse": "131 BV SEBASTOPOL 75002 PARIS ",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3526808,
+                48.8684467
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " ATOMIC CITY/MAJESTIC ",
+              "addresse": "48 Rue Alphonse Penaud  75020 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.4058568,
+                48.8689445
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " Restaurant Program ",
+              "addresse": "26 rue George Sand 75016 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2680449,
+                48.8501477
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+        }
+        
+      
+      
+          
