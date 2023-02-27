da2ac126cf0cef363c0a39e76ae969c086f6c77f
Azlou77 <nguyenlouis172@gmail.com>
2023-02-27 11:53:40 +0100

    add companies 94270

--- apimap/data/map.js
+++ apimap/data/map.js
@@ -1,39 +1,217 @@
-var mapJSON =
-{
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Isphers",
-          "color": "green",
-          "marker-size": "medium",
-          "amenity": "Baseball Stadium",
-          "popupContent": "This is where the Rockies play!"
-        },
-        "geometry": {
-          "coordinates": [
-            2.3578,
-            48.8123
-          ],
-          "type": "Point"
-        }
+var mapJSON = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Arts et images",
+        "addresse ": "19 Rue du general leclerc",
+        "color": "",
+        "marker-size": "medium",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
       },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Oderis",
-          "amenity": "companie client",
-          "popupContent": "This is where the Rockies play!",
-          "marker-color": "green",
-        },
-        "geometry": {
-          "coordinates": [
-            2.3371,
-            48.8712
-          ],
-          "type": "Point"
-        }
+      "geometry": {
+        "coordinates": [
+          2.35850072,
+          48.81274796
+        ],
+        "type": "Point"
       }
-    ]
-  }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Motoscoot",
+        "addresse ": "25 Bis Avenue Eugène Thomas",
+        "color": "",
+        "marker-size": "medium",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+      },
+      "geometry": {
+        "coordinates": [
+          4.422089795017769,
+          2.359499
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ETAPICA",
+        "addresse": "47 ter Rue Gabriel Péri",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.354116,
+          48.81286
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "TIMKL - LE KREMLIN BICETRE",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "addresse": "19 place Jean Jaurès"
+      },
+      "geometry": {
+        "coordinates": [
+          2.35695410,
+          48.81275177
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "StaffMe",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "addresse": "67 avenue de Fontainebleau"
+      },
+      "geometry": {
+        "coordinates": [
+          2.073000,
+          48.81471
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Résidence Roland Garros",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "addresse": "\t52 rue du Général Leclerc"
+      },
+      "geometry": {
+        "coordinates": [
+          2.358187,
+          48.812435
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "AMBULANCE DELATOUR",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "addresse": "21 RUE DU GENERAL LECLERC"
+      },
+      "geometry": {
+        "coordinates": [
+          2.3607,
+          48.8147
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "AEF COMPTABLE \t",
+        "addresse": "\t38 rue de la convention  ",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.35626507,
+          48.8121376
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Campanile Paris Sud - Porte d'Italie",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "addresse": "\t2 boulevard du Général de Gaulle"
+      },
+      "geometry": {
+        "coordinates": [
+          2.35906839,
+          48.81588745
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Carol Coiffure",
+        "addresse": "24 rue du Général Leclerc",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.359371,
+          48.813977
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "SCI OCCITANIENNE \t",
+        "addresse": "52 bis rue du Général Leclerc",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.189985421061351,
+          49.164044577335915
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "CABINET MEDICAL ROLAND GARROS",
+        "addresse": "16 Place Jean-Jaurès",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.357323,
+          48.812425
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+       "name": "Credit mutuel",
+        "addresse": "93, avenue de Fontainebleu",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium"
+      },
+      "geometry": {
+        "coordinates": [
+          2.36230612,
+          48.80958939
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
