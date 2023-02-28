5ff986e92a180e95f71fc1d3a4eca77b6ccaffac
Azlou77 <nguyenlouis172@gmail.com>
2023-02-28 12:34:00 +0100

    add data companies city93

---
+++ apimap/data/city93.js
@@ -0,0 +1,109 @@
+var city93JSON =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "KLM EQUIPEMENTS",
+          "addresse": "44 Rue Blaise Pascal, 93600 Aulnay-sous-Bois",
+          "color": "",
+          "marker-size": "medium",
+          "amenity": "",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4820017,
+            48.9498397
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Franco belge",
+          "addresse": "44 rue Maurice de Broglie, 93600 Aulnay-sous-Bois",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+      
+        },
+        "geometry": {
+          "coordinates": [
+            2.4846298,
+            48.9476263
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ATHENA GLOBAL SERVICES (distributeur exclusif ESET)",
+          "addresse": "20 All. Louis Calmanovic, 93320 Les Pavillons-sous-Bois",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.502929,
+            48.91051
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AFDE",
+          "addresse": "73 RUE JEAN LOLIVE 93100 MONTREUIL",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.429598,
+            48.867847
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Siemens S.A.S ",
+          "addresse": "9 Bd Finot, 93200 Saint-Denis",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.341534,
+            48.918292
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AGENCE ORPI (AGENCE DE LA MAIRIE)",
+          "addresse": "35 RUE DE PARIS 93100 MONTREUIL",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4304401,
+            48.8570395
+          ],
+          "type": "Point"
+        }
+      },
+    ]
+};
