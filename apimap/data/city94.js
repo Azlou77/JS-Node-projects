1a8ef88962a3eb42b89b6596ba7dd2e6d7c39fab
Azlou77 <nguyenlouis172@gmail.com>
2023-02-28 12:56:51 +0100

    add data companies city94

---
+++ apimap/data/city94.js
@@ -0,0 +1,179 @@
+var city94JSON =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Les Nouveaux Coursiers",
+          "addresse": "26 Rue Robert Witchitz, 94200 Ivry-sur-Seine",
+          "color": "",
+          "marker-size": "medium",
+          "amenity": "",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.402293,
+            48.8077321
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SGPF",
+          "addresse": "4 Rue Suchet, 94700 Maisons-Alfort",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+      
+        },
+        "geometry": {
+          "coordinates": [
+            2.447664,
+            48.805382
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " COBED ",
+          "addresse": "28 Quai des Carrières, 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4117256,
+            48.91051
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " DISTINCTIF",
+          "addresse": "85 RUE DU PETIT CHATEAU 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4029318,
+            48.8267665
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AMBRILLE ATPC CHARENTON",
+          "addresse": "40, rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4168609,
+            48.8191433
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bernard Ghnassia",
+          "addresse": "17, rue Maréchal Leclerc 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4122,
+            48.8221
+          ],
+          "type": "Point"
+        }
+      },
+       {
+        "type": "Feature",
+        "properties": {
+          "name": "CASTEELE",
+          "addresse": "31 rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4172852,
+            48.8188145
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ETUDE VAL 94",
+          "addresse": "68 BIS RUE DE PARIS 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4125725,
+            48.8224546
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Factory Club",
+          "addresse": "Entre StockOmani et Buffalo Grill, 3 Rue des Alouettes, 94320 Thiais ",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.3961,
+            48.76496
+          ],
+          "type": "Point"
+        }
+      },
+       {
+        "type": "Feature",
+        "properties": {
+          "name": "Mr. Toupoint",
+          "addresse": "60, rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4151979,
+            48.8204953
+          ],
+          "type": "Point"
+        }
+      },
+      
+    ]
+};
+    
