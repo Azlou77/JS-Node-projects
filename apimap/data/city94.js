5e43edbabadadccda4b95c14650e63acc9d2fa41
Azlou77 <nguyenlouis172@gmail.com>
2023-02-28 16:51:50 +0100

    add data companies near

---
+++ apimap/data/city94.js
@@ -0,0 +1,638 @@
+var city94JSON =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Les Nouveaux Coursiers",
+          "addresse": "26 Rue Robert Witchitz, 94200 Ivry-sur-Seine",
+          "color": "",
+          "marker-size": "medium",
+          "amenity": "",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.402293,
+            48.8077321
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SGPF",
+          "addresse": "4 Rue Suchet, 94700 Maisons-Alfort",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+      
+        },
+        "geometry": {
+          "coordinates": [
+            2.447664,
+            48.805382
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " COBED ",
+          "addresse": "28 Quai des Carrières, 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4117256,
+            48.91051
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " DISTINCTIF",
+          "addresse": "85 RUE DU PETIT CHATEAU 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4029318,
+            48.8267665
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AMBRILLE ATPC CHARENTON",
+          "addresse": "40, rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4168609,
+            48.8191433
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bernard Ghnassia",
+          "addresse": "17, rue Maréchal Leclerc 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4122,
+            48.8221
+          ],
+          "type": "Point"
+        }
+      },
+       {
+        "type": "Feature",
+        "properties": {
+          "name": "CASTEELE",
+          "addresse": "31 rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4172852,
+            48.8188145
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ETUDE VAL 94",
+          "addresse": "68 BIS RUE DE PARIS 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4125725,
+            48.8224546
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Factory Club",
+          "addresse": "Entre StockOmani et Buffalo Grill, 3 Rue des Alouettes, 94320 Thiais ",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.3961,
+            48.76496
+          ],
+          "type": "Point"
+        }
+      },
+       {
+        "type": "Feature",
+        "properties": {
+          "name": "Mr. Toupoint",
+          "addresse": "60, rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4151979,
+            48.8204953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ESSILOR",
+          "addresse": "147 RUE DE PARIS 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4052244,
+            48.8267911
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " RENNOTTE ",
+          "addresse": "162 RUE DE PARISMR LEFEBVRE 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4122,
+            48.8221
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "GARDIENNAGE PROTECTION SERVICE ",
+          "addresse": "3 RUE LABOURET 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4181828,
+            48.8217536
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "UNIVERSEL PHARMA ",
+          "addresse": "2 RUE DU NOUVEAU BERCY CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.396662,
+            48.8233539
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " SOMATA ",
+          "addresse": "97-99 RUE DE PARIS 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4143312,
+            48.8211031
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "GLODIS",
+          "addresse": "60, rue de Paris 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4092141,
+            48.8242289
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "DISTRIDYN    ",
+          "addresse": "18 AVENUE wINSTON CHURCHILL 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.402522,
+            48.8261
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SANCHEZ RETI",
+          "addresse": "27 Av. du Maréchal de Lattre de Tassigny, 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4198679,
+            48.8227262
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "CORCOS DU IMPLANTOLOGIE",
+          "addresse": "5 bis Rue du Port aux Lions, 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.3996932,
+            48.8239269
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "BI ACOUSTIQUE",
+          "addresse": "12 RUE DE VALMY  94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {
+          "coordinates": [
+            2.4033904,
+            48.8273632
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cabinet Dr. DAHAN Guy ",
+          "addresse": "11bis, place Jean Moulin 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4151979,
+            48.8229358
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cabinet Dr. DAHAN Guy ",
+          "addresse": "11bis, place Jean Moulin 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4151979,
+            48.8229358
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " EGE ",
+          "addresse": "13 Quai des Carrières, 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4171676,
+            48.8179412
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "HRS SOLUTION ",
+          "addresse": "35 Avenue de Maréchal de TASSIGNY",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.2755846,
+            48.8719646
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " CELEONET ",
+          "addresse": "70 Rue Berthie Albrecht, 94400 Vitry-sur-Seine",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.408469,
+            48.798621
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MCA TECHNOLOGY ",
+          "addresse": "ZA EUROPARC, 50 Rue Eugène Dupuis, 94000 Créteil",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4667,
+            48.7833
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " AIDAPAC",
+          "addresse": "8 QUAI DES CARRIERES 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.418439,
+            48.817933
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Renovedéco (Supdeco)",
+          "addresse": "5 Rue Eugène Delacroix, 94410 Saint-Maurice",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.420562,
+            48.8234142
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "GEHELLE-CHEVALLET",
+          "addresse": "21 avenue de Liège 94210 La Varenne Saint-Hilaire",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4932,
+            48.7939
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "GENERE",
+          "addresse": "5 av. de la Villa Anthony 94410 Saint-Maurice",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4272,
+            48.8218
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SAS HYMBERT IMMOBILIER",
+          "addresse": "1 Av. du 11 Novembre 1918, 94340 Joinville-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4746,
+            48.8254
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "PARTNERIMMO", 
+          "addresse": "7 RUE DE LA CONCORDE 94700 MAISONS ALFORT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.444434,
+            48.812025
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "AKORUS",
+          "addresse": "66 Rue de la Convention, 94700 Maisons-Alfort",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.450159    ,
+            48.813269
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ETS LOISEAU",
+          "addresse": "132 RUE DE PARIS 94220 CHARENTON LE PONT",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4064448,
+            48.8263649
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "GEHELLE",
+          "addresse": "21 Av. de Liège, 94210 Saint-Maur-des-Fossés",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4993118,
+            48.7912945
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "COBURN GROUP LIBERTE ",
+          "addresse": "18 Avenue Winston Churchill 94220 Charenton-le-Pont",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.402522,    
+            48.8261
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "COBURN GROUP RIS VINCENNES ",
+          "addresse": "37 rue des Vignerons 94300 Vincennes",
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "popupContent": ""
+        },
+        "geometry": {   
+          "coordinates": [
+            2.4311145    ,
+            48.842714
+          ],
+          "type": "Point"
+        }
+      },
+      
+    ]
+};
+    
