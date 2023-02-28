33a04774f683708df7c99768ed571ade92d8f5b9
Azlou77 <nguyenlouis172@gmail.com>
2023-02-28 11:57:52 +0100

    add data companies city92

---
+++ apimap/data/city92.js
@@ -0,0 +1,329 @@
+var city92JSON =
+{
+
+    "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "CMV MEDIFORCE",
+              "addresse": "123 Rue Jules Guesde, 92300 Levallois-Perret",
+              "color": "",
+              "marker-size": "medium",
+              "amenity": "",
+              "popupContent": ""
+            },
+            "geometry": {
+              "coordinates": [
+                2.2912172,
+                48.894833
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "SARL GHESTIA",
+              "addresse": "2 Rue du 8 Mai 1945, 92110 Clichy",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3105703,
+                48.8977239
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "DESTINY",
+              "addresse": "37/39, rue de Neuilly, 75002 Paris",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3016787,
+                48.9017389
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": " WESTERN DIGITAL",
+              "addresse": "4 Bd des Îles, 92130 Issy-les-Moulineaux",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2485977,
+                48.8238311
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "MORGANE DIFFUSION",
+              "addresse": "2 rue Liot de la Chavignerie, 92100 Boulogne-Billancourt",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.25,
+                48.8333
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Franco belge Malakoff ",
+              "addresse": "5-7, avenue Jules-Ferry 92240 Malakoff",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2989925,
+                48.8202768
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "HELIOSPHERES",
+              "addresse": "2 BOULEVARD JEAN JAURES 92100 BOULOGNE BILLANCOURT",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2423293590545654,
+                48.83573913574219
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "COSINUS",
+              "addresse": "3 Rue Danton, 92240 Malakoff",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                48.8223515,
+                48.8223515
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "EPS ERASME",
+              "addresse": "143 Av. Armand Guillebaud, 92160 Antony",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.3111915588378906,
+                48.74745178222656
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "ATL Transports",
+              "addresse": "17 Rue de la Station, 92600 Asnières-sur-Seine",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2850766,
+                48.9067382
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "DEGELPROD",
+              "addresse": "83 Rue Thiers, 92100 Boulogne-Billancourt",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.250094,
+                48.834776
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "KEYYO",
+              "addresse": "98 Av. Victor Hugo, 92100 Boulogne-Billancourt",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2471495,
+                48.8396397
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Transversal",
+              "addresse": "5 Rue Curie, 92150 Suresnes",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2338941,
+                48.8795774
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Iron Protection",
+              "addresse": "52 Rue de Longchamp, 92200 Neuilly-sur-Seine",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2549305,
+                48.8818317
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Renovedéco (Supdeco)",
+              "addresse": "5 rue Eugène Delacroix, 92200 Neuilly-sur-Seine",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2697,
+                48.8846
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "SEGINE - IMMOBILIERE DES VICTOIRES ASNIERES (SYNDIC)",
+              "addresse": "1 PLACE DES VICTOIRES 92600 ASNIERES SUR SEINE",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2909071,
+                48.9106305
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "GEOTECH",
+              "addresse": "9 Rue des Suisses, 92380 Garches",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.1967099,
+                48.8536652
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Schumacher et Associés",
+              "addresse": "9 Rue Berteaux Dumas, 92200 Neuilly-sur-Seine",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2749558,
+                48.8817632
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "GIACALONE NANTERRE",
+              "addresse": "6 RUE DES COURRIERES 92000 NANTERRE",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2295725,
+                48.9001941
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "DELPHINE PRODUCTION",
+              "addresse": "24 Bd Maillot, 92200 Neuilly-sur-Seine",
+              "marker-color": "#7e7e7e",
+              "marker-size": "medium"
+            },
+            "geometry": {
+              "coordinates": [
+                2.2762926,
+                48.8783894
+              ],
+              "type": "Point"
+            }
+          },
+        ]
+    };
