ab415c3281552a01fc53a164d97b494e7bf2d092
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-04-26 12:42:39 +0000

    view conditions, variables, displayMessages

---
+++ cours/variables.js
@@ -0,0 +1,12 @@
+// 3 ways to declare variables
+
+// var is only accessible in function
+   var j = 0
+
+ // let can be reassigned variable
+    let i = 0
+
+// const it's impossible
+   const k = 0
+
+
