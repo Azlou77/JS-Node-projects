7e167630ede354da1e1c6085c36740401f7e1ef0
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-12 14:15:12 +0000

    add base arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,13 @@
+const  weapons = ["sword", "spear", "revolver"] ;
+
+
+// find the lenght of array
+console.log(weapons.length)
+
+
+// getters and setters
+console.log(weapons[2])
+
+// set the value
+weapons[2] = "bow"
+console.log(weapons[2])
