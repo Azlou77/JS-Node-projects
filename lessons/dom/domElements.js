8d51b5910678a39e5941a665d7217cd4e6148b0f
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-13 07:37:19 +0000

    add arrays and dom notions

---
+++ lessons/dom/domElements.js
@@ -0,0 +1,13 @@
+/*  querySelector and querySelector all method
+    query selector: returns the first element that matches a specified CSS selector(s) in the document
+    query selector all: returns all elements in the document that matches a specified CSS selector(s), as a static NodeList object
+*/
+// get the h1 tag
+document.querySelector('h1'); 
+
+// get the button class
+document.querySelector('.btn'); 
+
+// getElementsById method
+// get the element with id myId
+document.getElementById('myId');
