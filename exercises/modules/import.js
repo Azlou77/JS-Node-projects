83decb0e8b716fb2bfdb0b5ad1e21ce50c052ba7
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-19 13:13:53 +0000

    add import js

---
+++ exercises/modules/import.js
@@ -0,0 +1,18 @@
+// Export an array
+let  weapons = ['candlestick', 'lead pipe', 'revolver'];
+
+// Export an constant
+const name = 'Alexander';
+
+// Export a class 
+class Character {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+}
+
+// export a function
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
