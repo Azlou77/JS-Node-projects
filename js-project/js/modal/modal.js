4f11edae87ec40ed6693741240c5cf48edce6497
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-25 10:50:33 +0000

    add modal overlay style

--- js-project/js/modal/modal.js
+++ js-project/js/modal/modal.js
@@ -8,14 +8,12 @@ const closeBtn = document.querySelector(".close-btn");
 // Events listeners
 modalBtn.addEventListener("click", function () {
       modal.classList.add("open-modal");
-   }
-
-);
+    
+});
 
 // Dismiss button
 closeBtn.addEventListener("click", function () {
       modal.classList.remove("open-modal");
-   }
-
-);
+      
+});
 
