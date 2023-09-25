c8f3d7520d23d03dcd4ef01391991392d0c1f791
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-25 10:18:37 +0000

    add JS modal

--- js-project/js/modal/modal.js
+++ js-project/js/modal/modal.js
@@ -1,2 +1,21 @@
 /* what is a modal a modal is
    an element that is displayed on top of the page an pop up */
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+
+// Events listeners
+modalBtn.addEventListener("click", function () {
+      modal.classList.add("open-modal");
+   }
+
+);
+
+// Dismiss button
+closeBtn.addEventListener("click", function () {
+      modal.classList.remove("open-modal");
+   }
+
+);
+
