53892c36837facd4e10baaca2240f185d20c0b8c
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-25 07:53:58 +0000

    add loop + text content Fixes #39

---
+++ lessons/functions/functions.js
@@ -0,0 +1,9 @@
+
+var totalDefense;
+
+function calculateDefense(helmet, armor) {
+  totalDefense = helmet + armor;
+  console.log(totalDefense);
+}
+calculateDefense(10,5);
+calculateDefense(20,10);
