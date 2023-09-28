84b9b0cd01bd92b4249815be269bdff7ead7e7a7
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-28 08:48:52 +0000

    add xhr function

--- cours/async functions/js/xhr.js
+++ cours/async functions/js/xhr.js
@@ -1,1 +1,32 @@
 // XHr for XML HTTP Request
+const log = document.querySelector(".event-log");
+
+// Add event listener to add text content
+document.querySelector("#xhr").addEventListener("click", () => {
+  log.textContent = "";
+
+  // Create a new XHR object
+  const xhr = new XMLHttpRequest();
+
+  // Add event listener to the XHR object
+  // Loadend is triggered when the request is finished
+  xhr.addEventListener("loadend", () => {
+    // Show the status of the request
+    log.textContent = `${log.textContent}Terminé avec le statut : ${xhr.status}`;
+  });
+
+  // Get the data from the URL
+  xhr.open(
+    "GET",
+    "https://raw.githubusercontent.com/mdn/translated-content/main/files/fr/_wikihistory.json",
+  );
+  // Send the request
+  xhr.send();
+  log.textContent = `${log.textContent}Requête XHR initiée\n`;
+});
+
+// Add event listener to reload
+document.querySelector("#recharger").addEventListener("click", () => {
+  log.textContent = "";
+  document.location.reload();
+});
