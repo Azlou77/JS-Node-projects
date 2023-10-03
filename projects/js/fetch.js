212a37266aae70a3f112ac2b91f56b3e8df96799
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-03 09:30:40 +0000

    update fetch Fixes #50

--- projects/js/fetch.js
+++ projects/js/fetch.js
@@ -1,10 +1,23 @@
+const tokenBearer = process.env.AIRTABLE_BEARER_TOKEN;
+const baseId = process.env.AIRTABLE_BASE_ID;
+const tableId = process.env.AIRTABLE_TABLE_ID;
+const url = `https://api.airtable.com/v0/${baseId}/${tableId}`;
+// Settings headers
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer " + tokenBearer);
+
 // Async function to list records from table Weapons with API Airtable
 async function listRecords() {
-    try {
-      const response = await fetch('https://www.example.com');
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
+    let response = await fetch(url, {
+        method: "GET",
+        headers: myHeaders,
+    });
+    if (response.ok) {
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let records = jsonResponse.records;
+        console.log(records);
+        return records;
     }
-  }
+    throw new Error("Request failed!");
+}
