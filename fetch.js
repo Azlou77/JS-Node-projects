7a39a6410c76bcad89268f0a2471336f7ae42ef3
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-03 10:27:45 +0000

    update 2 fetch

---
+++ fetch.js
@@ -0,0 +1,8 @@
+// fetch.js
+require('dotenv').config()
+
+
+console.log(process.env.AIRTABLE_BEARER_TOKEN)
+console.log(process.env.AIRTABLE_BASE_ID)
+
+
---
+++ projects/js/config.js
@@ -0,0 +1,5 @@
+// Export a config object with the keys and values
+const bearer_token = process.env.BEARER_TOKEN;
+const base_id = process.env.BASE_ID;
+
+export { bearer_token, base_id };
