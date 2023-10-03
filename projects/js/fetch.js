cffc00181275fcecf95b3a44efc0f44f23e78366
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-03 07:15:15 +0000

    Add asyn await function Fixes #50

--- projects/js/fetch.js
+++ projects/js/fetch.js
@@ -1,2 +1,10 @@
-// build.js
-console.log(`Hello ${process.env.HELLO}`)
+// Async function to list records from table Weapons with API Airtable
+async function listRecords() {
+    try {
+      const response = await fetch('https://www.example.com');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
