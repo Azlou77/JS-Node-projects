553fb1999cdecbf91fa3217ae1c807ad98acd88b
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-09 08:36:28 +0000

    update style banner, navbar, font

--- projects/js/fetch.js
+++ projects/js/fetch.js
@@ -1,7 +1,9 @@
+// Import config.js file
 import {key, base, table} from './config.js';
 
 const url = `https://api.airtable.com/v0/${base}/${table}`;
 
+// Fetch data from Airtable API
 fetch (url, {
     method: 'GET',
     mode: 'cors',
@@ -15,32 +17,49 @@ fetch (url, {
     return data.json();
 })
 .then(function(json) {
+    // Loop through the data
     let titles = json.records;
+
+    // Create a row
     let row = document.createElement('section');
     row.className = 'row';
     for (let i = 0; i < 4; i++) {
+
+        // Create a column
         let col = document.createElement('div');
         col.className = 'col-lg-2';
+
+        // Create a card
         let div = document.createElement('article');
         div.className = 'card';
+
+        // Create a card header
         let div1 = document.createElement('div');
         div1.className = 'card-header';
-        div1.innerHTML = titles[i].fields.Category;
+        div1.innerHTML = 'Category:' +' ' + titles[i].fields.Category;
+
+        // Create a card body
         let div2 = document.createElement('div');
+        // Create an image
         let img = document.createElement('img');
         img.className = 'card-img-top';
         img.src = titles[i].fields.Pictures[0].url;
         div2.className = 'card-body';
+
+        // Create a card title and text
         let h3 = document.createElement('h3');
         h3.className = 'card-title';
         h3.innerHTML = titles[i].fields.Name;
         let p = document.createElement('p');
         p.className = 'card-text';
         p.innerHTML = titles[i].fields.Description;
+
+        // Create a card footer
         let div3 = document.createElement('div');
         div3.className = 'card-footer';
-        div3.innerHTML = titles[i].fields.Prices + '€';
+        div3.innerHTML = 'Prices:' + ' ' + titles[i].fields.Prices + '€';
 
+        // Append elements to the DOM
         div2.appendChild(h3);
         div.appendChild(img);
         div2.appendChild(p);
@@ -53,6 +72,8 @@ fetch (url, {
     }
     document.body.appendChild(row);
 })
+
+// Catch errors
 .catch(function(error) {
   console.log(error);
 });
