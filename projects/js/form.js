51810c31cb32430ffe394a5057827c572c361e9b
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-12 08:53:25 +0000

    fix #65 bug and post new data in console

--- projects/js/form.js
+++ projects/js/form.js
@@ -1,31 +1,38 @@
 // Import config.js file
 import {key} from './config.js';
 
-const url = `https://api.airtable.com/v0/app708JRaOzw9xfPE/Weapons?view=GridW&api_key=${key}`;
+const url = `https://api.airtable.com/v0/app708JRaOzw9xfPE/Weapons`;
 
 let data = {
-    name: 'trident',
-    category: 'close combat',
-    description: 'a three-pronged spear',
-    image: 'https://i.imgur.com/9JjlfmS.jpg'
-};
+    "fields": {
+        "Link": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Warhammer_14th_century.jpg/800px-Warhammer_14th_century.jpg",
+        "Category": "close combat",
+        "Description": "A warhammer is a late medieval weapon of war intended for close combat action, whose design resembles the hammer.",
+        "Prices": 100,
+        "Name": "Warhammer"
 
+    }
+};
 // Fetch data from Airtable API
 fetch (url, {
     method: 'POST',
     mode: 'cors',
     headers: {
         'Authorization': `Bearer ${key}`,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
 })
-.then(function(data) {
-    return data.json();
-})
-// Success and error functions
-.then(function(json) {
-    console.log(json);
+.then((response) => {
+    if (response.ok) {
+        response.json().then((data) => {
+            console.log(data);                          
+          })
+    } else {
+        console.log('Erreur statut !=200');
+    }
+}).catch((error) => {
+    console.log(`Erreur: ${error.message}`);
 })
 
 
