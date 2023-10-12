3f4330a8b5006e195a4472199452fc11c785ac25
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-12 12:18:08 +0000

    add form to post data Fixes #67

--- projects/js/form2.js
+++ projects/js/form2.js
@@ -1,16 +1,19 @@
 // Import config.js file
 import {key, table, base} from './config.js';
 
-const url = `https://api.airtable.com/v0/${base}/${table}`;
 
-document.querySelector('button').addEventListener('click', getValue);
+document.getElementById('addBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    getValue();
+  });
+  
 function getValue() {
+ 
 
   // Select element by id to get value
   let name = document.querySelector('#name').value;
   let description = document.querySelector('#description').value;
   let category = document.querySelector('#category').value;
-  let price = document.querySelector('#price').value;
   let link = document.querySelector('#link').value;
 
   // Create object to store data
@@ -19,21 +22,33 @@ function getValue() {
           "Name": name,
           "Description": description,
           "Category": category, 
-          "Price": price,
           "Link": link
         }
-      }
-  }
+      };
 
+const url = `https://api.airtable.com/v0/${base}/${table}/`;
 
 fetch(url , {
   method: 'POST',
+  mode: 'cors',
   headers: {
     'Authorization': `Bearer ${key}`,
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    
   },
-  body: JSON.stringify(data)
+    body: JSON.stringify(data),
+ 
 })
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+.then((response) => {
+    if (response.ok) {
+        response.json().then((data) => {
+            console.log(data);
+                                       
+          })
+    } else {
+        console.log('Erreur statut !=200');
+    }
+}).catch((error) => {
+    console.log(`Erreur: ${error.message}`);
+})
+}
