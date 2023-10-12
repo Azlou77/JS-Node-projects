2bda6c1e97e2969ca1686adfe316eab0bce8b4d1
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-10-12 09:58:04 +0000

    select elements and define array object

--- projects/js/form2.js
+++ projects/js/form2.js
@@ -1,12 +1,30 @@
 // Import config.js file
 import {key, table, base} from './config.js';
 
-const url = `https://api.airtable.com/v0/${base}/${table}/`;
+const url = `https://api.airtable.com/v0/${base}/${table}`;
 
+document.querySelector('button').addEventListener('click', getValue);
+function getValue() {
 
-const form = document.querySelector('form');
-const formData = new FormData(form);
-const data = Object.fromEntries(formData.entries());
+  // Select element by id to get value
+  let name = document.querySelector('#name').value;
+  let description = document.querySelector('#description').value;
+  let category = document.querySelector('#category').value;
+  let price = document.querySelector('#price').value;
+  let link = document.querySelector('#link').value;
+
+  // Create object to store data
+  let data = {
+        "fields": {
+          "Name": name,
+          "Description": description,
+          "Category": category, 
+          "Price": price,
+          "Link": link
+        }
+      }
+  }
+
 
 fetch(url , {
   method: 'POST',
